6993f7e0b5908880348d5baf4cbd92c1e2c789be
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-11 13:12:30 -0600

    Dashboard route

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,7 +1,8 @@
+# Defines our Task Manager app
 class TaskManagerApp < Sinatra::Base
   set :root, File.expand_path('..', __dir__)
 
   get '/' do
-    'hello, world!'
+    erb :dashboard
   end
 end
