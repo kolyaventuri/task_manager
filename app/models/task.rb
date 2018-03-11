5b0514155d0af3198f9892bd38362b8f55623a18
Kolya Venturi <kolya.venturi@enx3s.com>
2018-03-11 14:13:47 -0600

    Individual task loading

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,11 +2,12 @@ require 'sqlite3'
 
 # Defines a task
 class Task
-  attr_reader :title, :description
+  attr_reader :title, :description, :id
 
   def initialize(task_params)
     @description = task_params['description']
     @title       = task_params['title']
+    @id = task_params["id"] if task_params["id"]
 
     @database = SQLite3::Database.new('db/task_manager_development.db')
     @database.results_as_hash = true
@@ -23,5 +24,12 @@ class Task
     tasks.map do |task|
       Task.new(task)
     end
+  end
+
+  def self.find(id)
+    database = SQLite3::Database.new('db/task_manager_development.db')
+    database.results_as_hash = true
+    task = database.execute('SELECT * FROM tasks WHERE id = ?', id.to_i).first
+    Task.new(task)
   end
 end
